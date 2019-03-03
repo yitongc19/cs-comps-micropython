f14b788a60d64f78e1550c1cbf58864812fc22a9
mangalds <35149431+mangalds@users.noreply.github.com>
2019-03-02 21:11:57 -0600

    Create one_led.py

---
+++ led/one_led.py
@@ -0,0 +1,24 @@
+# Taken from Github
+# https://github.com/ShahriyarR/Micro-Python-code-samples/blob/master/RGB_led.py
+
+import pyb
+from pyb import Pin
+
+#Blinking Singular color LED example
+
+Ledx1 = pyb.Pin('X1', pyb.Pin.OUT_PP, pyb.Pin.PULL_UP)
+
+LEDx1.low()
+
+try:
+  while True:
+  
+  LEDx1.value(False)
+  pyb.delay(1000)
+  
+  LEDx1.value(True)
+  pyb.delay(100)
+  
+  
+except:
+  LEDx1.value(False)
